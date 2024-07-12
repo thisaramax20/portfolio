53b9c05ef2fa86fb727f46974d78f4caca9c58a6
thisaramax20 <thisarathejana20@gmail.com>
2024-07-12 07:23:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    let menu = document.querySelector(".menu-links");
+    let icon = document.querySelector(".hamburger-icon");
+
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
